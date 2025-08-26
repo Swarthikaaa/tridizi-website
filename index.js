334bba31ae858ab45c79c32d69a22d89010c476d
Swarthikaaa <154448129+Swarthikaaa@users.noreply.github.com>
2025-08-26 12:37:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// Example: Simple form submission
+
 document.querySelector("form").addEventListener("submit", function(e){
     e.preventDefault();
     alert("Message Sent Successfully!");
   });
+
   
