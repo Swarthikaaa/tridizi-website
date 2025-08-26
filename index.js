dce1337f75ac9c08bdb4190bc19a1b4daa5115ca
Swarthikaaa <154448129+Swarthikaaa@users.noreply.github.com>
2025-08-26 12:20:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Example: Simple form submission
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message Sent Successfully!");
+  });
+  
